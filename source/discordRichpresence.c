589d59d6b1340fdfe0e771f395f8abd9cfd569a0
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-02-13 09:10:10 +0000

    added a plan to follow

--- source/discordRichpresence.c
+++ source/discordRichpresence.c
@@ -80,12 +80,16 @@ void update_presence(struct IDiscordActivityManager* activity_manager, struct Ap
     DiscordTimestamp start = 666;
     char large_text[] = "fishes";
     char large_image[] = "weeb_thing";
-    
+    char small_text[] = "happy lunar new year"; 
+    char small_image[] = "oxething";
+
     strncpy(activity.state, state, sizeof(activity.state));
     strncpy(activity.details, details, sizeof(activity.details));
     activity.timestamps.start = start; 
     strncpy(activity.assets.large_text, large_text, sizeof(activity.assets.large_text)); 
     strncpy(activity.assets.large_image, large_image, sizeof(activity.assets.large_image));
+    strncpy(activity.assets.small_text, small_text, sizeof(activity.assets.small_text)); 
+    strncpy(activity.assets.small_image, small_image, sizeof(activity.assets.small_image));
 
     callback callback_presence = &callback_func;
     activity_manager -> update_activity(activity_manager, &activity,app.core,callback_presence);
